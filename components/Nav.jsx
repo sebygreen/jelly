0130d900aee7a69b851bcaf78a9337e8c3f8ed0a
Sebastien Green <45818412+sebygreen@users.noreply.github.com>
2024-02-19 19:54:16 +0100

    added nav indicator

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,29 +3,46 @@
 import Link from "next/link";
 import styles from "@/style/Nav.module.css";
 import { usePathname } from "next/navigation";
+import { useEffect, useRef } from "react";
 
 export default function Nav() {
     const pathname = usePathname();
+    const nav = useRef(null);
+
+    useEffect(() => {
+        const active = nav.current.querySelector(".active");
+        const indicator = nav.current.querySelector(`.${styles.indicator}`);
+        if (active) {
+            const values = {
+                width: active.offsetWidth,
+                offset: active.offsetLeft,
+            };
+            indicator.style.width = `${values.width}px`;
+            indicator.style.transform = `translateX(${values.offset}px)`;
+            indicator.style.opacity = 1;
+        } else {
+            indicator.style.opacity = 0;
+        }
+    }, [pathname]);
+
     return (
-        <nav>
-            <Link
-                className={pathname === "/" ? styles.active : undefined}
-                href="/"
-            >
+        <nav ref={nav} className={styles.container}>
+            <Link className={pathname === "/" ? "active" : undefined} href="/">
                 Home
             </Link>
             <Link
-                className={pathname === "/about" ? styles.active : undefined}
+                className={pathname === "/about" ? "active" : undefined}
                 href="/about"
             >
                 About
             </Link>
             <Link
-                className={pathname === "/projects" ? styles.active : undefined}
+                className={pathname === "/projects" ? "active" : undefined}
                 href="/projects"
             >
                 Projects
             </Link>
+            <div className={styles.indicator} />
         </nav>
     );
 }
