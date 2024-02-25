8ff7cc2228faac73ced9e36efd92ba3e9462d4ed
Loïc <loic.schussler@epita.fr>
2024-02-25 15:55:09 +0100

    feat(3-menu_burger): ajout d'un menu hamburger sur mobile

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
+"use client";
+
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import HeaderMobile from "@/components/HeaderMobile";
+
+import { useState, useEffect } from "react";
 
 import localFont from "next/font/local";
 const satoshi = localFont({
@@ -20,17 +25,33 @@ const satoshi = localFont({
     ],
 });
 
-export const metadata = {
+const metadata = {
     title: "Jelly Agency",
     description:
         "Votre Expert en Stratégies Digitales pour Restaurants et Entreprises Innovantes.",
 };
 
 export default function RootLayout({ children }) {
+    const [isMobile, setIsMobile] = useState(false);
+
+    // Effect to check screen width
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsMobile(window.innerWidth < 768); // For example, 768px is a common breakpoint for mobile devices
+        };
+
+        // Check on mount and on window resize
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+
+        // Cleanup
+        return () => window.removeEventListener('resize', checkScreenSize);
+    }, []);
+
     return (
         <html lang="en">
             <body className={satoshi.className}>
-                <Header />
+                {isMobile ? <HeaderMobile /> : <Header />}
                 {children}
                 <Footer />
             </body>
